8b6815ca638c1a9be3bd6072e34593fb593bb55e
huybk213 <huybk213@gmail.com>
2022-04-05 15:14:53 +0700

    bug in flash

---
+++ MDK-ARM/RTE/_USB_MSC_Test/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'USB_MSC_Test' 
+ * Target:  'USB_MSC_Test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
